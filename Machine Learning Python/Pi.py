9f15568ba7c2be0260e3efe18a37d800922f4787
dlindsay23 <lindsay.devinj@gmail.com>
2022-02-16 02:40:58 +0000

    Pi addition

--- Machine Learning Python/Pi.py
+++ Machine Learning Python/Pi.py
@@ -1,2 +1,31 @@
-# Python Program to find the L.C.M. of two input number
+import random
+from tabulate import tabulate
 
+
+
+radius = 1
+trials = [100, 1000, 10000, 100000, 1000000, 10000000]
+
+
+table = []
+
+for trial in trials:
+    
+    inside = 0
+
+    for i in range(trial):
+        
+        x = random.random()
+        y = random.random()
+        
+        
+        if x**2 + y**2 <= radius**2:
+            inside += 1
+        
+    area = 4*inside/trial
+    
+    table.append([trial, area])
+    
+
+    
+print(tabulate(table, headers=["Trials","Pi"]))
